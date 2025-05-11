90de1eb330b9e560f31a29c7cbf26517fd5cd622
chiragkhimani <khimanichirag@gmail.com>
2025-05-11 21:50:57 +0530

    My Changes

--- src/test/java/com/automation/pages/TestPage.java
+++ src/test/java/com/automation/pages/TestPage.java
@@ -3,5 +3,6 @@ package com.automation.pages;
 public class TestPage {
     public static void main(String[] args) {
         System.out.println("My name is Chirag");
+        System.out.println("Hello My World");
     }
 }
